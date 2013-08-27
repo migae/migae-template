2a0fcf619f96763a1945d4e8367778b086754d0f
Gregg Reynolds <dev@mobileink.com>
2013-08-27 12:04:38 -0500

    bump version to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject migae/lein-template "0.1.0"
+(defproject migae/lein-template "0.1.1"
   :description "migae project template"
   :url "http://github.com/greynolds/migae-template"
   :license {:name "Eclipse Public License"
