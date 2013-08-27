a2eba6a705816fe1eda7c8e2cd3ba4fc2349d072
Gregg Reynolds <dev@mobileink.com>
2013-08-27 12:02:41 -0500

    fix url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject migae/lein-template "0.1.0"
   :description "migae project template"
-  :url "http://github.com/greynolds/migae"
+  :url "http://github.com/greynolds/migae-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
