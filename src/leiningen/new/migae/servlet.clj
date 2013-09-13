0b78f70c7d6bf2c91ee203026680f0255ce54a66
Gregg Reynolds <dev@mobileink.com>
2013-09-12 21:02:54 -0500

    fix servlet templates

--- src/leiningen/new/migae/servlet.clj
+++ src/leiningen/new/migae/servlet.clj
@@ -1,11 +1,11 @@
-(ns {{appname}}.{{servlet}}-servlet
+(ns {{appname}}.{{-servlet}}-servlet
     (:gen-class :extends javax.servlet.http.HttpServlet)
-    (:require [{{appname}}.{{servlet}}-impl :as impl]
+    (:require [{{appname}}.{{-servlet}}-impl :as impl]
             [ring.util.servlet :as ring]))
 
 (defn -service
   [this rqst resp]
     (let [request-map  (ring/build-request-map rqst)
-          response-map (impl/{{servlet}}-handler request-map)]
+          response-map (impl/{{-servlet}}-handler request-map)]
     (when response-map
       (ring/update-servlet-response resp response-map))))
--- src/leiningen/new/migae/servlet_impl.clj
+++ src/leiningen/new/migae/servlet_impl.clj
@@ -1,20 +1,20 @@
-(ns {{appname}}.{{servlet}}-impl
+(ns {{appname}}.{{-servlet}}-impl
   (:use compojure.core
         [ring.middleware.params :only [wrap-params]]
         [ring.middleware.file-info :only [wrap-file-info]])
   (:require [compojure.route :as route]))
 
-(defroutes {{servlet}}-routes
+(defroutes {{-servlet}}-routes
   {{#services}}
   (GET "{{url-pattern}}" []
-    (str (format "<h1>Ohayo from {{ns}} servlet path {{url-pattern}}!</h1>")
+    (str (format "<h1>Ohayo from {{appname}}.{{-servlet}}-impl servlet path {{url-pattern}}!</h1>")
          "\n\n<a href='/'>home</a>"))
   {{/services}}
 
   (route/not-found "<h1>Page not found</h1>"))
 
-(def {{servlet}}-handler
-  (-> #'{{servlet}}-routes
+(def {{-servlet}}-handler
+  (-> #'{{-servlet}}-routes
       wrap-params
       wrap-file-info
       ))
