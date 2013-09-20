e16aebe735639799043b311181c3b001bf705445
Gregg Reynolds <dev@mobileink.com>
2013-09-19 19:09:18 -0500

    bump version to 0.2.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject migae/lein-template "0.2.4"
+(defproject migae/lein-template "0.2.5"
   :description "migae project template"
   :url "http://github.com/greynolds/migae-template"
   :source-paths ["src" "src/main/clojure"]
